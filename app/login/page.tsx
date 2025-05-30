7348a387f4f5f38bdbe3aca499098e0b99db5fb9
= <ahmedjawad1857@gmail.com>
2025-05-30 21:15:34 +0500

    fix: add margin to login header for improved spacing

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ const Login = () => {
       data-lk-theme="default"
       className="flex items-center flex-col justify-center bg-[var(--lk-bg)] min-w-screen min-h-screen"
     >
-      <h1 className="font-bold text-3xl text-center text-white">Login to continue</h1>
+      <h1 className="font-bold text-3xl text-center text-white mb-5">Login to continue</h1>
       <LoginForm />
     </div>
   );
