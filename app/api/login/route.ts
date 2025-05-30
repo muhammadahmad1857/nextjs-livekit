4a67620700bae6ed8d136b13792c0ea5390225b3
= <ahmedjawad1857@gmail.com>
2025-05-30 21:08:10 +0500

    fix: improve login response handling and set cookie expiration

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,12 +1,21 @@
 import { NextRequest } from "next/server";
 
-export const POST = async (req:NextRequest)=>{
-    
+export const POST = async (req: NextRequest) => {
+  const { email, password } = await req.json();
 
-const { email, password } = await req.json();
-if (email === process.env.USER_EMAIL && password === process.env.USER_PASSWORD) {
-  return new Response('Login successful', { status: 200, headers: { 'Set-Cookie': `session=true` } });
-} else {
-  return new Response('Invalid email or password', { status: 401 });
-}
-}
+  if (
+    email === process.env.USER_EMAIL &&
+    password === process.env.USER_PASSWORD
+  ) {
+    const expires = new Date(Date.now() + 60 * 60 * 1000).toUTCString(); // 1 hour
+
+    return new Response("Login successful", {
+      status: 200,
+      headers: {
+        'Set-Cookie': `session=true; Path=/; HttpOnly; SameSite=Lax; Expires=${expires}`,
+      },
+    });
+  } else {
+    return new Response("Invalid email or password", { status: 401 });
+  }
+};
